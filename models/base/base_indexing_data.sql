53897c4788e500c981245a7cf725782a087e88e5
subritt <subrittburlakoti@gmail.com>
2024-02-09 01:16:16 +0545

    added schema.yml. Changed columns header name to match the standard naming.

--- models/base/base_indexing_data.sql
+++ models/base/base_indexing_data.sql
@@ -1,13 +1,39 @@
+/*
+Data type, column name transformation
+*/
+
 {{ 
     config(
         materialized='table'
     )
  }}
 
-with
-raw_quality_data as (
+with raw_quality_data as (
     select *
     from {{ source('quality_data', 'sample_sr_raw_data') }}
 ),
+/*
+InvoiceID, IndexingStep2, ClientCode, Indexer, ChangedDate_EST,
+InvoiceNumber, InvoiceDate, Amount, Company, Vendor, AccountNumber, DueDate,
+SR_URL1
+*/
+/* changed column header */
+change_column_header_name as (
+    select 
+        "InvoiceID" as invoice_id,
+        "IndexingStep2" as indexing_label,
+        "ClientCode" as client_code,
+        "Indexer" as indexer_srcfid,
+        "ChangedDate_EST" as changed_date_est,
+        "InvoiceNumber" as invoice_number,
+        "InvoiceDate" as invoice_date,
+        "Amount" as amount,
+        "Company" as compay,
+        "Vendor" as vendor,
+        "AccountNumber" as account_number,
+        "DueDate" as due_date,
+        "SR_URL1" as invoice_url
+    from raw_quality_data
+)
 
-select * from raw_quality_data
+select * from change_column_header_name
