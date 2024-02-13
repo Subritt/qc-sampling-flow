41b8b2ae54025687fbcae0166bc4912f2759f1be
subritt <subrittburlakoti@gmail.com>
2024-02-14 00:12:53 +0545

    created stg_latest_entry model to only factor in the lates entry

---
+++ models/staging/stg_latest_entry.sql
@@ -0,0 +1,50 @@
+/*
+- removing columns that are not required.
+- re-arranging the columns as per the requirement.
+*/
+
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+-- referencing stg_indexing_raw_data model
+with 
+get_stg_indexing_data as (
+    select
+        invoice_id,
+        indexing_label,
+        client_code,
+        indexer_srcf_id,
+        changed_at,
+        invoice_number,
+        invoice_date,
+        amount,
+        company,
+        vendor,
+        account_number,
+        due_date,
+        invoice_url,
+        entry_version
+    from {{ ref('stg_indexing_data') }}
+),
+
+remove_remaining_columns as (
+    select
+        invoice_url,
+        invoice_date,
+        company,
+        amount,
+        vendor,
+        invoice_number,
+        account_number,
+        due_date,
+        client_code,
+        invoice_id,
+        indexer_srcf_id
+    from get_stg_indexing_data
+    where entry_version = 1
+)
+
+select * from remove_remaining_columns
