29f58a9dd968b4b03465aed0a9f6ee215d6bf08c
subritt <subrittburlakoti@gmail.com>
2024-02-13 23:21:03 +0545

    pr review resolution. Changed  seed csv file header, added logic to rank distict windows in  stg_indexing_data.sql

--- models/base/base_indexing_data.sql
+++ models/base/base_indexing_data.sql
@@ -9,7 +9,20 @@ Data type, column name transformation
  }}
 
 with raw_quality_data as (
-    select *
+    select
+       invoice_id,
+       indexing_step_2,
+       client_code,
+       indexer,
+       changed_date_est,
+       invoice_number,
+       invoice_date,
+       amount,
+       company,
+       vendor,
+       account_number,
+       due_date,
+       sr_url1
     from {{ source('quality_data', 'sample_sr_raw_data') }}
 ),
 /*
@@ -22,27 +35,26 @@ SR_URL1
 /* changed column header */
 change_column_header_name as (
     select 
-        "InvoiceID" as invoice_id,
-        "IndexingStep2" as indexing_label,
-        "ClientCode" as client_code,
-        "Indexer" as indexer_srcfid,
-        "ChangedDate_EST"::timestamp as changed_date_est,
-        "InvoiceNumber" as invoice_number,
-        "InvoiceDate" as invoice_date,
-        "Amount" as amount,
-        "Company" as compay,
-        "Vendor" as vendor,
-        "AccountNumber" as account_number,
-        "DueDate" as due_date,
-        "SR_URL1" as invoice_url
+        invoice_id,
+        indexing_step_2 as indexing_label,
+        client_code,
+        indexer as indexer_srcf_id,
+        changed_date_est::timestamp as changed_at, -- time in UTC
+        invoice_number,
+        invoice_date,
+        amount,
+        company,
+        vendor,
+        account_number,
+        due_date,
+        sr_url1 as invoice_url
     from raw_quality_data
 ),
 
 add_random_number_column as (
     select
-        *,
-        RANDOM() as random_number
-    from raw_quality_data
+        *
+    from change_column_header_name
 )
 
 select * from add_random_number_column
